18768558a33510d717e66a5578c956e71f7c137b
Carmen Caniglia <carmencaniglia@gmail.com>
2024-02-12 19:23:39 +0100

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,30 @@
+import { Col, Container, Row } from "react-bootstrap";
+import image from "../assets/immagini/home1.png";
+
+const Home = () => {
+  return (
+    <Container fluid>
+      <Row className="my-5 d-flex align-items-center justify-content-center">
+        <Col lg={6} md={12} className="d-flex">
+          <img alt="trainer" src={image} className="img-home" />
+
+          <div className="d-flex align-items-center text-center">
+            <p>
+              Noi di Exedra ci impegnamo a fornire un ambiente accogliente e
+              motivante dove persone di tutte le età e livelli di fitness
+              possono raggiungere i loro obiettivi di salute e benessere. Con
+              allenatori esperti, attrezzature all&apos; avanguardia e una vasta
+              gamma di classi e programmi, siamo qui per guidarti nel tuo
+              viaggio verso una vita più sana e attiva. Siamo più di una
+              palestra, siamo una comunità dedicata al miglioramento personale e
+              al sostegno reciproco. Unisciti a noi e inizia il tuo percorso
+              verso una versione più forte, più felice e più sana di te stesso!
+            </p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Home;
---
+++ src/components/MyNavbar.jsx
@@ -0,0 +1,63 @@
+import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
+import logo from "../assets/loghi/LOGO copia 2.png";
+
+const MyNavbar = () => {
+  return (
+    <Container fluid>
+      <Row>
+        <Navbar>
+          <Col xs={4} md={3} lg={2} className="d-flex align-items-center">
+            <Navbar.Brand href="#home">
+              <img
+                alt="logo Exedra"
+                src={logo}
+                width="150"
+                height="50"
+                className="ms-3"
+              />
+            </Navbar.Brand>
+          </Col>
+          <Col xs={8} md={6} lg={8}>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse
+              id="basic-navbar-nav"
+              className="justify-content-evenly"
+            >
+              <Nav>
+                <Nav.Link href="#home" className="mx-3">
+                  Home
+                </Nav.Link>
+                <Nav.Link href="#servizi" className="mx-3">
+                  Servizi
+                </Nav.Link>
+                <Nav.Link href="#abbonamenti" className="mx-3">
+                  Abbonamenti
+                </Nav.Link>
+                <Nav.Link href="#corsi" className="mx-3">
+                  Corsi
+                </Nav.Link>
+                <Nav.Link href="#shop" className="mx-3">
+                  Shop
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Col>
+          <Col
+            xs={12}
+            md={3}
+            lg={2}
+            className="d-flex justify-content-end align-items-center"
+          >
+            <Nav className="justify-content-end me-3">
+              <Nav.Link href="#login">Login</Nav.Link>
+              <div className="separatore"> | </div>
+              <Nav.Link href="#registrati">Registrati</Nav.Link>
+            </Nav>
+          </Col>
+        </Navbar>
+      </Row>
+    </Container>
+  );
+};
+
+export default MyNavbar;
